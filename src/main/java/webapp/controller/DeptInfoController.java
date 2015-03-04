98078edf6c8b68b71d86d31ea5d0f41bd4eb3959
hjji <jhjdk@naver.com>
2015-03-04 15:54:10 +0900

    xxx

--- src/main/java/webapp/controller/DeptInfoController.java
+++ src/main/java/webapp/controller/DeptInfoController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.catalina.connector.Request;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
